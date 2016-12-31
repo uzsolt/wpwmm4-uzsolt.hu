6849debb285ac6ebfb6e652c1e81fb77e709f370
zsolt <zsolt@9c950c4f-a2b8-e711-a987-005056a07bed>
2016-12-31 14:17:04 +0000

    duplázások kiszűrése
    git-svn-id: file:///usr/local/subversion/uzsolt.hu/wpwmm4-uzsolt.hu/trunk@39 9c950c4f-a2b8-e711-a987-005056a07bed

--- scripts/list_comp_pdf.sh
+++ scripts/list_comp_pdf.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Parameters:
-# $1 - filename (without data/ directory)
+# $1 - filename (without data/ directory and .psv suffix)
 # $2 - optional, which field
 # $3 - the value
 
@@ -9,14 +9,13 @@
 # field 2 => evfolyam
 
 if [ -n "$2" ]; then
-  AWKSTR="\$$2==$3 {print \$0}"
+  AWKSTR="\$$2==\"$3\" {print \$0}"
 else
   AWKSTR="{print \$0}"
 fi
-echo ${AWKSTR}
 
 IFS="|"
-cat data/$1 | awk -F "${IFS}" ${AWKSTR} | while read tanev evf descr path; do
-  tanevstr="${tanev}-$((tanev+1)). tanév"
+cat data/$1.psv | awk -F "${IFS}" ${AWKSTR} | while read tanev evf descr path; do
+  tanevstr="${tanev}-$((tanev+1))"
   echo "_verseny_pdf(\`${path}',\`${tanevstr}',\`${evf}',\`${descr}')"
 done
