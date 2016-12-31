bb8c9b4ff2864c32e2395fb5c11965b4b362a9f8
zsolt <zsolt@9c950c4f-a2b8-e711-a987-005056a07bed>
2016-12-31 11:57:16 +0000

    TAB törlése
    git-svn-id: file:///usr/local/subversion/uzsolt.hu/wpwmm4-uzsolt.hu/trunk@33 9c950c4f-a2b8-e711-a987-005056a07bed

--- comp_lists.mk
+++ comp_lists.mk
@@ -1,16 +1,16 @@
 COMP_LISTS= \
-			comp/math/aranyd \
-			comp/math/bolyai \
-			comp/math/imolya \
-			comp/math/kalmar \
-			comp/math/nemzetkozi \
-			comp/math/nlg \
-			comp/math/oktv \
-			comp/math/szfvi \
-			comp/phys/borpal \
-			comp/phys/budo \
-			comp/phys/mikola \
-			comp/phys/tornyai
+	comp/math/aranyd \
+	comp/math/bolyai \
+	comp/math/imolya \
+	comp/math/kalmar \
+	comp/math/nemzetkozi \
+	comp/math/nlg \
+	comp/math/oktv \
+	comp/math/szfvi \
+	comp/phys/borpal \
+	comp/phys/budo \
+	comp/phys/mikola \
+	comp/phys/tornyai
 
 .for C in ${COMP_LISTS}
 CTANEV_${C}!=cut -d , -f 1 data/${C}.csv | sort -u
