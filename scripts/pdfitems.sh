ce4081c06a8d371bb401902446b822819bafa203
zsolt <zsolt@9c950c4f-a2b8-e711-a987-005056a07bed>
2017-01-28 17:30:42 +0000

    uzsolt.hu-val egyenlező static/... szerkezet
    git-svn-id: file:///usr/local/subversion/uzsolt.hu/wpwmm4-uzsolt.hu/trunk@49 9c950c4f-a2b8-e711-a987-005056a07bed

--- scripts/pdfitems.sh
+++ scripts/pdfitems.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 IFS="|"
+DIR=`echo ${1} | sed "s,/[^/]*,/static/stuff,"`
 cat data/${1}.psv | while read txt path tooltip; do
-  echo "_own_pdf(\`${txt}',\`${path}',\`${tooltip}')"
+  echo "_own_pdf(\`${txt}',\`${DIR}/${path}',\`${tooltip}')"
 done
 
