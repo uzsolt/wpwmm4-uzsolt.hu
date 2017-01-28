7d01f67246f251d4fe2b842aa8d53b065fbca5ea
zsolt <zsolt@9c950c4f-a2b8-e711-a987-005056a07bed>
2017-01-28 17:07:28 +0000

    PSV fájl használata a menüben Perl-szkript helyett shell szkript generate_menu.mk egyszerűsítése
    git-svn-id: file:///usr/local/subversion/uzsolt.hu/wpwmm4-uzsolt.hu/trunk@47 9c950c4f-a2b8-e711-a987-005056a07bed

--- generate_menu.mk
+++ generate_menu.mk
@@ -1,23 +1,10 @@
-MENUDIR=menu/
+MENUDIR=menu
 MENUFILE=menu.gen
-.for V in ${VIRTUALS}
-TMENUTARGET:=${VIRTUALDIR_${V}}
-TMENUTARGET:=${TMENUTARGET:M*}
-TMENUTARGET:=${TMENUTARGET} ${VIRTUALOUT_${V}:S/^/${VIRTUALDIR_${V}}/:H:=/}
-MENUTARGET:=${MENUTARGET} ${TMENUTARGET}
-.endfor
-MENUTARGET:=${MENUTARGET:S/.//g:O:u:C/^ *\///g}
-MENUTARGETX:=${MENUTARGET}
-.for X in 1 2 3 4 5 6
-MENUTARGETX:=${MENUTARGETX:S/\/$//g:H:H:S/.//g}
-MENUTARGET:=${MENUTARGET} ${MENUTARGETX}
-.endfor
-MENUTARGET:=${MENUTARGET:S/.//g:C/^ *\///g:S/\/$//g:O:u}
-MENUTARGET:=${MENUTARGET:S/^/${MENUDIR}/:S/$/\/${MENUFILE}/}
+MENUDATAFILE=data/menu.psv
+MENUTARGET!=sed -n '/^\// s,^\([^|]*\).*,${MENUDIR}\1${MENUFILE},p' ${MENUDATAFILE}
+MENUTARGET:=${MENUDIR}/${MENUFILE} ${MENUTARGET}
 
-generate-menu: ${MENUTARGET}
-
-${MENUTARGET}: data/menu.json scripts/genmenu.pl
+${MENUTARGET}: data/menu.psv scripts/genmenu.sh
 	@mkdir -p ${.TARGET:S/${MENUFILE}$//}
-	@scripts/genmenu.pl /${.TARGET:S/^${MENUDIR}//:S/${MENUFILE}$//} > ${.TARGET}
+	@scripts/genmenu.sh ${.TARGET:S/^${MENUDIR}//:S/${MENUFILE}$//} > ${.TARGET}
 
---
+++ scripts/genmenu.sh
@@ -0,0 +1,66 @@
+#!/bin/sh
+
+IFS="|"
+FILE=data/menu.psv
+STR_HOMEPAGE="Kezdőlap"
+
+print_menu() {
+
+  local level=${1:-0} ; shift
+  local current=${1:-""} ; shift
+  # level+2: the first character is /
+  local next_level=`echo ${current} | cut -d / -f -$((level+2))`
+
+  if [ ${level} -eq 0 ]; then
+    local pattern='^http://\|^/[^/]*/|'
+  else
+    local current_level=`echo ${current} | cut -d / -f -$((level+1))`
+    local pattern="${current_level}/[^/]*/|"
+  fi
+
+  local indentstr=`jot -b ' ' -s '' $((level*2+2))`
+  grep "${pattern}" ${FILE} | \
+    while read href title tooltip; do
+      if echo ${href} | grep -q "^${next_level}/$"; then
+        local divclass=selected
+      else
+        if [ "${level}" -eq 0 ]; then
+          local divclass=menu
+        else
+          local divclass=submenu
+        fi
+      fi
+      printf "%s_DIV(%s,\n" "${indentstr}" ${divclass}
+      printf "  %s\`_HREF(\`%s\',\`%s\',\`class=menu_href',\`%s\')" "${indentstr}" "${href}" "${title}" "${tooltip}"
+      if echo ${href} | grep -q "^${next_level}/$"; then
+        print_menu $((level+1)) ${current}
+      fi
+      printf "'\n%s)\n" "${indentstr}"
+    done
+
+}
+
+# kilistázza a gyökérben levő pontokat (0. szint)
+# közben megtalálja a belépési pontot a következő szintre
+#   grep-pel az előző szintre
+#   közben megtalálja a belépési pontot a következő szintre...
+cat << EOF
+<div id=menu>
+  <a href=http://uzsolt.hu>
+  <div id=menu_header><h2>Udvari Zsolt honlapja</h2></div>
+  </a>
+  <div class=div_menu>
+EOF
+
+print_menu 0 $*
+cat << EOF
+  </div>
+</div>
+EOF
+if [ "${1}" = "" ]; then
+  ACTIVE_MENU="${STR_HOMEPAGE}"
+else
+  ACTIVE_MENU=`grep "$1|" ${FILE} | awk -F '|' '{print $2}'`
+fi
+echo "m4_define(_ACTIVEMENU,\`${ACTIVE_MENU}')"
+
