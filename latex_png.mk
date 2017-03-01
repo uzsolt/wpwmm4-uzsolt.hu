b3f6b0491fb1d5101c3c6984dd4e0c3a1120d46c
zsolt <zsolt@9c950c4f-a2b8-e711-a987-005056a07bed>
2017-03-01 05:56:36 +0000

    png-fájlok generálása ciklusban
    git-svn-id: file:///usr/local/subversion/uzsolt.hu/wpwmm4-uzsolt.hu/trunk@84 9c950c4f-a2b8-e711-a987-005056a07bed

--- latex_png.mk
+++ latex_png.mk
@@ -2,14 +2,8 @@ TEMPDIR=./temp/
 PDFLATEX=pdflatex -output-directory=${TEMPDIR} ${.ALLSRC} > /dev/null
 CONVERT=convert ${.TARGET:C,.*/,${TEMPDIR},:R}.pdf ${.TARGET}
 
-${DEST_DIR}notes/latex/angles.png: ${SRC_DIR}notes/latex/angles.tex
+.for PNG in angles grid segments
+${DEST_DIR}notes/latex/${PNG}.png: ${SRC_DIR}notes/latex/${PNG}.tex
 	@${PDFLATEX}
 	@${CONVERT}
-
-${DEST_DIR}notes/latex/grid.png: ${SRC_DIR}notes/latex/grid.tex
-	@${PDFLATEX}
-	@${CONVERT}
-
-${DEST_DIR}notes/latex/segments.png: ${SRC_DIR}notes/latex/segments.tex
-	@${PDFLATEX}
-	@${CONVERT}
+.endfor
