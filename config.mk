9c5e7da532ad4af2bde759d4557d30f5a295d095
zsolt <zsolt@9c950c4f-a2b8-e711-a987-005056a07bed>
2017-03-01 17:28:45 +0000

    psv-fájl legyen függősége a keplist-nek
    git-svn-id: file:///usr/local/subversion/uzsolt.hu/wpwmm4-uzsolt.hu/trunk@88 9c950c4f-a2b8-e711-a987-005056a07bed

--- config.mk
+++ config.mk
@@ -88,6 +88,7 @@ VIRTUALOUT_keplist= grafika/index.html \
 					personal/summer2012/index.html \
 					personal/summer2014/index.html
 VIRTUALREQ_keplist= scripts/list_thumbs.sh
+VIRTUALREQRULE_keplist=C,(.*)/index.html,data/${VIRTUALDIR_keplist}\1.psv,
 
 VIRTUALOUT_menu= index.html \
 				 edu/index.html \
