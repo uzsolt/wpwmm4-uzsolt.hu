a5385a81386ca27b0a7f38f287fb803369925ba5
zsolt <zsolt@9c950c4f-a2b8-e711-a987-005056a07bed>
2017-03-02 20:09:21 +0000

    := értékadás
    git-svn-id: file:///usr/local/subversion/uzsolt.hu/wpwmm4-uzsolt.hu/trunk@93 9c950c4f-a2b8-e711-a987-005056a07bed

--- comp_lists.mk
+++ comp_lists.mk
@@ -6,5 +6,7 @@ COMP_LISTS!=sed -n \
 CTANEV_${C}!=cut -d \| -f 1 data/${C}.psv | sort -u
 CEVF_${C}!=cut -d \| -f 2 data/${C}.psv | sort -u
 VIRTUALOUT_list_evf_pdf+=${CEVF_${C}:S/^/${C}\//:=.html}
+VIRTUALOUT_list_evf_pdf:=${VIRTUALOUT_list_evf_pdf}
 VIRTUALOUT_list_tanev_pdf+=${CTANEV_${C}:S/^/${C}\//:=.html}
+VIRTUALOUT_list_tanev_pdf:=${VIRTUALOUT_list_tanev_pdf}
 .endfor
