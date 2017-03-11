afc4e8983b8f30b7c4c07aad09b1338336862200
zsolt <zsolt@9c950c4f-a2b8-e711-a987-005056a07bed>
2017-03-11 15:11:46 +0000

    cím hozzáadása a thumb-listhez
    git-svn-id: file:///usr/local/subversion/uzsolt.hu/wpwmm4-uzsolt.hu/trunk@98 9c950c4f-a2b8-e711-a987-005056a07bed

--- scripts/list_thumbs.sh
+++ scripts/list_thumbs.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 IFS="|"
+sed -n "\,${1}, s,.*|\(.*\),<center><h2>\1</h2></center>,p" data/menu.psv
 cat data/${1}.psv | while read pict descr; do
   printf '  <div class="thumb">\n'
   printf '    <a href="%s"><img width=64 height=64 src="%s" alt="%s"/></a>\n' "${pict}.html" "/static/${1}/thumb/${pict}" "${descr}"
