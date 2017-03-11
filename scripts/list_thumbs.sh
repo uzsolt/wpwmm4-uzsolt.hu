2300bdaff163656f3143a5e5c235ad3b39ffeab0
zsolt <zsolt@9c950c4f-a2b8-e711-a987-005056a07bed>
2017-03-11 14:59:11 +0000

    add /static/, thumb/ href
    git-svn-id: file:///usr/local/subversion/uzsolt.hu/wpwmm4-uzsolt.hu/trunk@96 9c950c4f-a2b8-e711-a987-005056a07bed

--- scripts/list_thumbs.sh
+++ scripts/list_thumbs.sh
@@ -3,7 +3,7 @@
 IFS="|"
 cat data/${1}.psv | while read pict descr; do
   printf '  <div class="thumb">\n'
-  printf '    <a href="%s"><img width=64 height=64 src="%s" alt="%s"/></a>\n' "${pict}.html" "${pict}" "${descr}"
+  printf '    <a href="%s"><img width=64 height=64 src="%s" alt="%s"/></a>\n' "${pict}.html" "/static/${1}/thumb/${pict}" "${descr}"
   printf '    <div class="thumb_desc">%s</div>\n' "${descr}"
   printf '  </div>\n'
 done
