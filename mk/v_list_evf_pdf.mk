4a94e7803946e127a89cfc0d72ed1a48cdf30c7d
zsolt <zsolt@9c950c4f-a2b8-e711-a987-005056a07bed>
2017-03-31 15:11:00 +0000

    include/common.m4 req
    git-svn-id: file:///usr/local/subversion/uzsolt.hu/wpwmm4-uzsolt.hu/trunk@123 9c950c4f-a2b8-e711-a987-005056a07bed

--- mk/v_list_evf_pdf.mk
+++ mk/v_list_evf_pdf.mk
@@ -1,4 +1,4 @@
 VIRTUALTEMPLATE_list_evf_pdf=list_evf_pdf
 VIRTUALDIR_list_evf_pdf=
-VIRTUALREQ_list_evf_pdf=scripts/list_comp_pdf.sh
+VIRTUALREQ_list_evf_pdf=scripts/list_comp_pdf.sh include/common.m4
 
