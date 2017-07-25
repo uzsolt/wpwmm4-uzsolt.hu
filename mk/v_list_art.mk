f6ee0c21fb21b201e0d850f52b283354920816d7
zsolt <zsolt@9c950c4f-a2b8-e711-a987-005056a07bed>
2017-07-25 15:21:16 +0000

    Fizikai Szemle cikkek
    git-svn-id: file:///usr/local/subversion/uzsolt.hu/wpwmm4-uzsolt.hu/trunk@161 9c950c4f-a2b8-e711-a987-005056a07bed

--- mk/v_list_art.mk
+++ mk/v_list_art.mk
@@ -5,6 +5,12 @@ VIRTUALOUT_list_art_pdf= atom/index.html \
 												 elektr/index.html \
 												 em/index.html \
 												 feny/index.html \
-												 fiztori/index.html
+												 fiztori/index.html \
+												 hang/index.html \
+												 mech/index.html \
+												 meres/index.html \
+												 modern/index.html \
+												 ped/index.html \
+												 relelm/index.html
 VIRTUALREQ_list_art_pdf=scripts/list_art_pdf.sh
 VIRTUALREQRULE_list_art_pdf=C,(.*)/index.html,data/edu/phys/art/\1.psv,
