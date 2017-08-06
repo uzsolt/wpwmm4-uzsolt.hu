4aefe1b1ea50e1e24500d58743959489ccb0a1ad
zsolt <zsolt@9c950c4f-a2b8-e711-a987-005056a07bed>
2017-08-06 12:21:02 +0000

    kép megtekintésekor elrendezés változtatás
    git-svn-id: file:///usr/local/subversion/uzsolt.hu/wpwmm4-uzsolt.hu/trunk@165 9c950c4f-a2b8-e711-a987-005056a07bed

--- scripts/kep.sh
+++ scripts/kep.sh
@@ -26,15 +26,15 @@ grep -C 1 "$2|" data/${1}.psv | ( while read fn descr; do
   fi
 done
 
-printf '  <center><h3>%s</h3></center>\n' "${curr_descr}"
-printf '  <center>\n'
-  [ -n "${prev_fn}" ] && a_img "${prev_fn}.html" "/static/pict/icons/go-previous.png" "Előző"
-  a_img "/${1}" "/static/pict//icons/go-home.png" "Vissza"
-  [ -n "${next_fn}" ] && a_img "${next_fn}.html" "/static/pict/icons/go-next.png" "Következő"
-printf '  </center>\n'
+printf '  <div class="imgview">\n'
+printf '    <div class="imgdesc">%s</div>\n' "${curr_descr}"
+printf '    <a href="%s"><img src="%s" alt="%s" class="imgviewed"/></a>\n' "/static/$1/${curr_fn}" "/static/$1/${curr_fn}" "${curr_descr}"
 
-printf '  <center>\n'
-printf '    <a href="%s"><img src="%s" alt="%s"/></a>\n' "/static/$1/${curr_fn}" "/static/$1/${curr_fn}" "${curr_descr}"
-printf '  </center>\n'
+printf '  <div class="imgnav">\n'
+  [ -n "${prev_fn}" ] && a_img "${prev_fn}.html" "/static/pict/icons/go-previous.png" "Előző"
+  a_img "/${1}/" "/static/pict/icons/go-home.png" "Vissza"
+  [ -n "${next_fn}" ] && a_img "${next_fn}.html" "/static/pict/icons/go-next.png" "Következő"
+printf '  </div>\n'
+printf '  </div>\n'
 
 )
