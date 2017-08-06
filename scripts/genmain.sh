a9bb413e1c107838195df5cdec93e73853b5d131
zsolt <zsolt@9c950c4f-a2b8-e711-a987-005056a07bed>
2017-08-06 20:00:11 +0000

    felesleges br eltávolítása
    git-svn-id: file:///usr/local/subversion/uzsolt.hu/wpwmm4-uzsolt.hu/trunk@179 9c950c4f-a2b8-e711-a987-005056a07bed

--- scripts/genmain.sh
+++ scripts/genmain.sh
@@ -12,7 +12,7 @@ fi
 
 process() {
   printf '  <a href="%s"><div class="note_stuff" title="%s">\n' "${url}" "${title}"
-  printf '    <span class="note_date">%s</span><span class="note_title">%s</span></br>\n' "${date}" "${descr}"
+  printf '    <span class="note_date">%s</span><span class="note_title">%s</span>\n' "${date}" "${descr}"
   printf '  </div></a>\n'
 }
 
