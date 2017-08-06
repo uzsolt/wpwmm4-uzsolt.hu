8176defd8c7efb18737492bab9504cd7ce32c524
zsolt <zsolt@9c950c4f-a2b8-e711-a987-005056a07bed>
2017-08-06 15:40:24 +0000

    feljegyzések listájának szépítése
    git-svn-id: file:///usr/local/subversion/uzsolt.hu/wpwmm4-uzsolt.hu/trunk@170 9c950c4f-a2b8-e711-a987-005056a07bed

--- scripts/list_notes.sh
+++ scripts/list_notes.sh
@@ -1,18 +1,13 @@
 #!/bin/sh
 
 . scripts/functions
-echo '<table id="note_table">'
 int=0
 
 process() {
-  printf '   <tr class=row%s>\n' "$((int+1))"
-  printf '    <td class=col_date>%s</td>\n' "${date}"
-  printf '    <td class=col_href><a href="%s" title="%s">%s</a></td>\n' "${url}" "${descr}" "${title}"
-  printf '    <td class=col_desc>%s</td>\n' "${descr}"
-  printf '   </tr>\n'
-  int=$(((int+1)%2))
+  printf '  <a href="%s"><div class="note_stuff" title="%s">\n' "${url}" "${title}"
+  printf '    <span class="note_date">%s</span><span class="note_title">%s</span></br>\n' "${date}" "${title}"
+  printf '    <div class="note_descr">%s</div>\n' "${descr}"
+  printf '  </div></a>\n'
 }
 
 read_data data/${1}.psv process title descr date url
-
-echo '  </table>'
