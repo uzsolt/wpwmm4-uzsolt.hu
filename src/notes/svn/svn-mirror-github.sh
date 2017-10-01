bd5d9659575c84612c1e2129f20ba6c9225b6eb3
zsolt <zsolt@9c950c4f-a2b8-e711-a987-005056a07bed>
2017-10-01 14:53:56 +0000

    git push --set-upstream sorral bővítés
    git-svn-id: file:///usr/local/subversion/uzsolt.hu/wpwmm4-uzsolt.hu/trunk@208 9c950c4f-a2b8-e711-a987-005056a07bed

--- src/notes/svn/svn-mirror-github.sh
+++ src/notes/svn/svn-mirror-github.sh
@@ -1,6 +1,7 @@
 cd /path/to/git/
-git svn init -s http://svn.yourdomain.com/repos/project_name
+git svn init -s http://svn.yourdomain.com/repos/project_name project_name
 cd project_name
 git svn fetch
 
 git remote add origin git@github.com:USERNAME/PROJECT
+git push --set-upstream origin master
