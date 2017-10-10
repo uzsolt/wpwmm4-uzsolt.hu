901adf6643e49f2f02a8addc6fe1fec4bff1e883
zsolt <zsolt@9c950c4f-a2b8-e711-a987-005056a07bed>
2017-10-10 17:03:02 +0000

    tidy5-höz -q opció, hogy a hibamentes HTML-fájlhoz ne írjon ki üzenetet.
    git-svn-id: file:///usr/local/subversion/uzsolt.hu/wpwmm4-uzsolt.hu/trunk@218 9c950c4f-a2b8-e711-a987-005056a07bed

--- config.mk
+++ config.mk
@@ -13,7 +13,7 @@ LAYOUT_DIR=layout/
 # Virtual directory
 VIRT_DIR=virtual/
 
-HOOK_POST_HTML=${MSG2} Checking validity ; tidy5 -e -f /dev/stdout ${.TARGET} | sed '/About/,$$d ; s,^,     ,'
+HOOK_POST_HTML=${MSG2} Checking validity ; tidy5 -q -e -f /dev/stdout ${.TARGET} | sed '/About/,$$d ; s,^,     ,'
 HOOK_POST_VHTML=${HOOK_POST_HTML}
 
 # Targets in ${DEST_DIR}
