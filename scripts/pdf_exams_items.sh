081b2777970664a6bd2d3d9cbe832130f6f2551b
zsolt <zsolt@9c950c4f-a2b8-e711-a987-005056a07bed>
2017-10-10 16:20:55 +0000

    Feladatsorok listájának h1 és h2 tag-je ne a sablon legyen.
    A "Feladatsorok" ill. "Gyakorló feladatsorok évfolyamonként csoportosítva"
    helyett "<Tantárgy> feladatsorok" és "<Évfolyam>. évfolyam"
    
    
    git-svn-id: file:///usr/local/subversion/uzsolt.hu/wpwmm4-uzsolt.hu/trunk@217 9c950c4f-a2b8-e711-a987-005056a07bed

--- scripts/pdf_exams_items.sh
+++ scripts/pdf_exams_items.sh
@@ -2,6 +2,15 @@
 
 . scripts/functions
 
+if echo "${1}" | grep -q '/math/'; then
+  title="Matematika"
+else
+  title="Fizika"
+fi
+title="${title} feladatsorok"
+
+printf '<h1>%s</h1>\n' "${title}"
+printf '<h2>%s. évfolyam</h2>\n' ${2}
 DIR=`echo ${1}${2} | sed "s,/[^/]*,/static/stuff,"`
 process() {
   echo "_own_pdf(\`${txt}',\`${DIR}/${path}',\`${tooltip}')"
