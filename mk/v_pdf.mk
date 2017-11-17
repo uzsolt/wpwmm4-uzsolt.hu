a360522bfac484651cda50cd832bbaa0b6c13e92
zsolt <zsolt@9c950c4f-a2b8-e711-a987-005056a07bed>
2017-11-17 15:11:20 +0000

    Introduce years in guides too (not only exams). Use exams/10 and guide/10 in static instead of exams10 (without /) Small simplify in scripts/pdf_exams_items.sh script.
    git-svn-id: file:///usr/local/subversion/uzsolt.hu/wpwmm4-uzsolt.hu/trunk@239 9c950c4f-a2b8-e711-a987-005056a07bed

--- mk/v_pdf.mk
+++ mk/v_pdf.mk
@@ -2,8 +2,6 @@ VIRTUALTEMPLATE_pdf=pdfitems
 VIRTUALDIR_pdf=
 VIRTUALOUT_pdf=	downloads/dox/index.html \
 				downloads/linuxvilag/index.html \
-				edu/math/guide/index.html	\
-				edu/phys/guide/index.html \
 				edu/eco/index.html
 VIRTUALREQ_pdf+=scripts/pdfitems.sh
 VIRTUALREQRULE_pdf=C,(.*)/index.html,data/\1.psv,
--- scripts/pdf_exams_items.sh
+++ scripts/pdf_exams_items.sh
@@ -2,16 +2,29 @@
 
 . scripts/functions
 
-if echo "${1}" | grep -q '/math/'; then
-  title="Matematika"
-else
-  title="Fizika"
-fi
-title="${title} feladatsorok"
+case `echo ${1} | awk -F / '{print $3}'` in
+  math)
+    title="Matematika"
+    ;;
+  phys)
+    title="Fizika"
+    ;;
+esac
+
+case `echo ${1} | awk -F / '{print $4}'` in
+  exams)
+    titlecat="feladatsorok"
+    ;;
+  guide)
+    titlecat="segédletek"
+    ;;
+esac
+
+title="${title} ${titlecat}"
 
 printf '<h1>%s</h1>\n' "${title}"
 printf '<h2>%s. évfolyam</h2>\n' ${2}
-DIR=`echo ${1}${2} | sed "s,/[^/]*,/static/stuff,"`
+DIR=`echo ${1}/${2} | sed "s,/[^/]*,/static/stuff,"`
 process() {
   echo "_own_pdf(\`${txt}',\`${DIR}/${path}',\`${tooltip}')"
 }
