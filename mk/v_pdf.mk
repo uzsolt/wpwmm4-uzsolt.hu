c209146797a86460d2accfa9ada3cd3e059917df
zsolt <zsolt@9c950c4f-a2b8-e711-a987-005056a07bed>
2017-12-03 17:42:08 +0000

    edu/sch/ menüpont Sávválasztás, egyetemi/főiskolai továbbtanulás, többletpontok
    git-svn-id: file:///usr/local/subversion/uzsolt.hu/wpwmm4-uzsolt.hu/trunk@246 9c950c4f-a2b8-e711-a987-005056a07bed

--- mk/v_pdf.mk
+++ mk/v_pdf.mk
@@ -2,6 +2,7 @@ VIRTUALTEMPLATE_pdf=pdfitems
 VIRTUALDIR_pdf=
 VIRTUALOUT_pdf=	downloads/dox/index.html \
 				downloads/linuxvilag/index.html \
-				edu/eco/index.html
+				edu/eco/index.html \
+				edu/sch/index.html
 VIRTUALREQ_pdf+=scripts/pdfitems.sh
 VIRTUALREQRULE_pdf=C,(.*)/index.html,data/\1.psv,
