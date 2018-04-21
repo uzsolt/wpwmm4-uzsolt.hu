347868ffc46804243d942de737dbf66a46add748
zsolt <zsolt@9c950c4f-a2b8-e711-a987-005056a07bed>
2018-04-21 09:03:09 +0000

    </br> -> <br>
    git-svn-id: file:///usr/local/subversion/uzsolt.hu/wpwmm4-uzsolt.hu/trunk@389 9c950c4f-a2b8-e711-a987-005056a07bed

--- scripts/list_notes.sh
+++ scripts/list_notes.sh
@@ -4,7 +4,7 @@
 
 process() {
   printf '  <a href="%s"><div class="note_stuff" title="%s">\n' "${url}" "${title}"
-  printf '    <span class="note_date">%s</span><span class="note_title">%s</span></br>\n' "${date}" "${title}"
+  printf '    <span class="note_date">%s</span><span class="note_title">%s</span><br>\n' "${date}" "${title}"
   printf '    <div class="note_descr">%s</div>\n' "${descr}"
   printf '  </div></a>\n'
 }
