dab41870465d92d9be7f69191b7abdd847d9818f
zsolt <zsolt@9c950c4f-a2b8-e711-a987-005056a07bed>
2019-02-18 10:06:56 +0000

    Nemzetiségi pótlék százaléka 15 -> 30
    git-svn-id: file:///usr/local/subversion/uzsolt.hu/wpwmm4-uzsolt.hu/trunk@518 9c950c4f-a2b8-e711-a987-005056a07bed

--- mk/notes_other.mk
+++ mk/notes_other.mk
@@ -1,4 +1,6 @@
 TARGETS+=	\
 	notes/other/gyerek.html \
 	notes/other/pedagogus_ber.html
-notes/other/pedagus_ber.html_REQ=scripts/ped_tabla_1.sh
+notes/other/pedagus_ber.html_REQ+=	\
+	scripts/ped_tabla_0.sh \
+	scripts/ped_tabla_1.sh
