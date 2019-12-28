0d39a2e939e07d9b3535cfd55f251e97208170b0
zsolt <zsolt@9c950c4f-a2b8-e711-a987-005056a07bed>
2019-12-28 13:57:19 +0000

    Reszponzív kiterjesztés - kezdeti verzió. A div-es táblák reszponzívak.
    git-svn-id: file:///usr/local/subversion/uzsolt.hu/wpwmm4-uzsolt.hu/trunk@602 9c950c4f-a2b8-e711-a987-005056a07bed

--- mk/css.mk
+++ mk/css.mk
@@ -3,6 +3,7 @@ ${DEST_DIR}css/main.css: \
 	assets/css/main.css \
 	assets/css/_image.scss \
 	assets/css/_menu.scss \
+	assets/css/_responsive.scss \
 	assets/css/_stuff.scss \
 	assets/css/_syntax.scss \
 	assets/css/_table.scss
